552ff03ebe406be138dced7e554f93528ca018a6
ibn-ahmad04 <mouhammad.dioum04@gmail.com>
2024-12-02 22:38:30 +0000

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=aMFlGJRJ#nDle1llp9icYvllCifo5Rd8r6HH7vpF-JqIg7WJPwo4",
 };
